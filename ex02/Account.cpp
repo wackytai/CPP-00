af743a09c91d94b5c973a39fa8be7b69dc87d3ce
wackytai <tainahmonteiro.info@gmail.com>
2023-10-05 12:00:52 +0100

    setting account.cpp functions

---
+++ ex02/Account.cpp
@@ -0,0 +1,61 @@
+#include "Account.hpp"
+
+Account::Account( int initial_deposit )
+{
+
+}
+
+Account::~Account( void )
+{
+
+}
+
+static int	getNbAccounts( void )
+{
+
+}
+
+static int	getTotalAmount( void )
+{
+
+}
+
+static int	getNbDeposits( void )
+{
+
+}
+
+static int	getNbWithdrawals( void )
+{
+
+}
+
+static void	displayAccountsInfos( void )
+{
+
+}
+
+void	Account::makeDeposit( int deposit )
+{
+
+}
+
+bool	Account::makeWithdrawal( int withdrawal )
+{
+
+}
+
+int		Account::checkAmount( void ) const
+{
+
+}
+
+void	Account::displayStatus( void ) const
+{
+
+}
+
+static void	_displayTimestamp( void )
+{
+
+}
